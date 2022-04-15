900abca586a2b9eb0a8b9853550ce592197e85dd
JIOjosBG <jjjooosssiiifff@gmail.com>
2022-04-15 22:04:45 +0300

    update to forms

--- server/pages/forms.py
+++ server/pages/forms.py
@@ -1,7 +1,12 @@
 from django.forms import ModelForm
+from django.forms.widgets import TextInput
 from api.models import Alarm
 
 class AlarmForm(ModelForm):
     class Meta:
         model = Alarm
         fields = ['title','time','color']
+        widgets = {
+            'color': TextInput(attrs={'type': 'color'}),
+        }
+        
