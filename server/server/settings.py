b680cb288cf01ac06d6c7fb8cfaaecfe251c9d53
JIOjosBG <jjjooosssiiifff@gmail.com>
2022-04-15 20:59:43 +0300

    no more api

--- server/server/settings.py
+++ server/server/settings.py
@@ -55,7 +55,9 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    
     'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -68,6 +70,7 @@ MIDDLEWARE = [
 # ]
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ORIGIN_ALLOW_ALL = True
+
 ROOT_URLCONF = 'server.urls'
 
 TEMPLATES = [
