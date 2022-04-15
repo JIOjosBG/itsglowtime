2b0acd3b7371bff09a03fa2a2b8bc6f8664748ef
JIOjosBG <jjjooosssiiifff@gmail.com>
2022-04-15 19:41:34 +0300

    api work in the frontedn

--- server/server/settings.py
+++ server/server/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'pages',
 
     'rest_framework',
+    'corsheaders'
 ]
 
 # REST_FRAMEWORK = {
@@ -54,11 +55,18 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
+
+# CORS_ALLOWED_ORIGINS = [
+#     r"^*",
+# ]
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'server.urls'
 
